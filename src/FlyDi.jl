b1a1c5e522a399327d0fbe9e33c8fb9ef322e572
RESheldon <48218792+RESheldon@users.noreply.github.com>
2019-11-29 11:29:50 +0000

    First major Ps beta version

--- src/FlyDi.jl
+++ src/FlyDi.jl
@@ -1,14 +1,18 @@
-module FlyDi
+module FlyPs
 
-export c, q, u, m_e, h, a0, Ry, kB,
+export c, q, u, m_e, h, a0, Ry, kB, m_Ps, a_Ps, Ry_Ps, E_hPs, hbar,
     FastAdjust,
     h5read_pa, potential,
-    grid_r, electrode_g, pa_g, potential_g, field_g, amp_field_g, grad_field_g,
+    grid_r, electrode_g, pa_g, potential_g, field_g, amp_field_g, grad_field_g, lost_g,
     kinetic_energy, potential_energy,
+	k_uniform, k_laser,
     euler, leapfrog, rk4,
+	ion_classic_MaxLFS, ion_none, ion_rates, E_Stark,
     traj, traj_last,
-    flydi, flydi_last,
-    mc_supersonic, mc_uniform
+    flydi, flydi_last, flydi_Para,
+    mc_supersonic, mc_uniform, Ps_formation, fluence, ls_delta_lambda, lineshape,
+	ls_fluence, ls_intensity, ls_power, ls_sigma_wl, ls_bandwidth_wl, ls_peak_intensity, ls_peak_power, ls_area, Ps_Laser_Slice,
+	MakDir, DFSave, DFOpen, acceleration
 
 include("external.jl")
 include("constants.jl")
@@ -16,5 +20,6 @@ include("fastadjust.jl")
 include("traj.jl")
 include("fly.jl")
 include("montecarlo.jl")
+include("SiManage.jl")
 
 end
